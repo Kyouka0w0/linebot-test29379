6a4a79012c51e0cdacf17f5c447d48206fe77ca7
Kyouka0w0 <askarte0w0@gmail.com>
2020-10-02 16:38:22 +0900

    修正

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -11,15 +11,16 @@ class LinebotController < ApplicationController
   end
 
   def callback
-      body = request.body.read
-      signature = request.env['HTTP_X_LINE_SIGNATURE']
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
-      head :Bad_Request
+      head :bad_request
     end
-  
+
     events = client.parse_events_from(body)
-  
-    events.each do |event|
+
+    events.each { |event|
       case event
       when Line::Bot::Event::Message
         case event.type
@@ -29,9 +30,10 @@ class LinebotController < ApplicationController
             text: event.message['text']
           }
           client.reply_message(event['replyToken'], message)
+        end
       end
-    end
-  end
+    }
 
-  head :OK
+    head :ok
+  end
 end
