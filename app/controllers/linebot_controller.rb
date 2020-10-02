5835052a1f3183dadc8be51475dfb047174a55fc
Kyouka0w0 <askarte0w0@gmail.com>
2020-10-02 20:52:38 +0900

    userid実装

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -2,14 +2,7 @@ class LinebotController < ApplicationController
   require 'line/bot'
 
   protect_from_forgery :except => [:callback]
-
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-  end
-
+  
   def callback
     body = request.body.read
 
@@ -19,11 +12,12 @@ class LinebotController < ApplicationController
     end
 
     events = client.parse_events_from(body)
-
     events.each { |event|
       case event
-      when Line::Bot::Event::Message
+      
+      when Line::Bot::Event::Message #ユーザーからメッセージが送信された場合(1)
         case event.type
+        #テキスト形式の場合
         when Line::Bot::Event::MessageType::Text
           message = {
             type: 'text',
@@ -31,9 +25,25 @@ class LinebotController < ApplicationController
           }
           client.reply_message(event['replyToken'], message)
         end
+      
+      when Line::Bot::Event::Follow #友だち追加された場合(2)
+        #登録したユーザーのidをlinebotテーブルに格納
+        line_id = event['source']['userId']
+        User.create(line_id: line_id)
+      when Line::Bot::Event::Unfollow #ブロックしたユーザーのデータを削除(3)
+        #お友達解除したユーザーのデータをユーザーテーブルから削除
+        line_id = event['source']['userId']
+        User.find_by(line_id: line_id).destroy
       end
     }
-
     head :ok
+  end
+
+  private
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
   end
 end
