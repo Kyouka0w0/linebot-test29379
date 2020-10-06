41e7b3f24b747bbd891f2794455b4cf425a366bd
Kyouka0w0 <askarte0w0@gmail.com>
2020-10-06 11:18:00 +0900

    微調整

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -39,7 +39,7 @@ class LinebotController < ApplicationController
                 "明日は雨が降るみたい、傘が必要かも\n 現在の降水確率\n 6〜12時 #{per06to12}%\n 12〜18時 #{per12to18}％\n 18〜24時 #{per18to24}％"
             else
               push =
-                "明日は雨は降らないと思う"
+                "明日は雨、降らないと思う"
             end
           when /.*(明後日|あさって).*/
             per06to12 = doc.elements[xpath + 'info[3]/rainfallchance/period[2]l'].text
@@ -50,7 +50,7 @@ class LinebotController < ApplicationController
                 "明後日、雨が降るかも"
             else
               push =
-                "明後日、雨は降らないと思う"
+                "明後日は雨、降らないと思う"
             end
           when /.*(可愛い|かわいい|好き|すき).*/
             push =
